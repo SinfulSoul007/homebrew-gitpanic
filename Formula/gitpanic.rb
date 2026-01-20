1e92504a9970d253b364e83272c202332d726d0d
Manit Garg <manit.garg.in@gmail.com>
2026-01-19 22:45:04 -0500

    Update formula with correct SHA256 after npm publish

--- Formula/gitpanic.rb
+++ Formula/gitpanic.rb
@@ -2,7 +2,7 @@ class Gitpanic < Formula
   desc "Big friendly buttons for common Git disasters - Terminal CLI"
   homepage "https://github.com/SinfulSoul007/gitpanic-cli"
   url "https://registry.npmjs.org/gitpanic-cli/-/gitpanic-cli-0.1.0.tgz"
-  sha256 "placeholder_sha256_will_be_updated_after_npm_publish"
+  sha256 "06709e477435ffb3d074926157e55eb62bef4c8f8081ca3e4aad6fae10a0ff8c"
   license "MIT"
 
   depends_on "node"
